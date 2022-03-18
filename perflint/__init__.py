1054c90ba11fe2255e96420bd5fe14a5eb62cc5c
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-03-18 16:33:20 +1100

    bump version

--- perflint/__init__.py
+++ perflint/__init__.py
@@ -6,7 +6,7 @@ from perflint.for_loop_checker import ForLoopChecker, LoopInvariantChecker
 if TYPE_CHECKING:
     from pylint.lint import PyLinter
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 def register(linter: "PyLinter") -> None:
